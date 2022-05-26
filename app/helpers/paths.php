5fb04ee1c1c055f9c25fe6fa43f73214368706db
Jonathan Roy <jroy66@gmail.com>
2022-05-26 01:42:57 -0400

    new helper for theme public path

--- app/helpers/paths.php
+++ app/helpers/paths.php
@@ -5,3 +5,8 @@ function theme_path($path = '')
     $siteTheme = explode(':', config('site.theme'), 2)[0];
     return base_path("vendor/{$siteTheme}/{$path}");
 }
+
+function theme_public_path($path = '')
+{
+    return public_path("theme/{$path}");
+}
