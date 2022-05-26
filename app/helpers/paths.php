4b846d065f8b790875f318515b501ab517d70bf2
Jonathan Roy <jroy66@gmail.com>
2022-05-26 12:30:21 -0400

    improve theme install/remove/update with symlink creation

--- app/helpers/paths.php
+++ app/helpers/paths.php
@@ -1,12 +1,26 @@
 <?php
 
-function theme_path($path = '')
+function theme_path($path = '', $theme = null)
 {
-    $siteTheme = explode(':', config('site.theme'), 2)[0];
-    return base_path("vendor/{$siteTheme}/{$path}");
+    $theme ??= config('site.theme');
+    $theme = explode(':', $theme, 2)[0];
+
+    if (isset($theme) && strlen($theme) > 0) {
+        return !empty($path)
+            ? base_path("vendor/{$theme}/{$path}")
+            : base_path("vendor/{$theme}");
+    }
 }
 
-function theme_public_path($path = '')
+function theme_public_path($path = '', $theme = null)
 {
-    return public_path("theme/{$path}");
+    $theme ??= config('site.theme');
+    $theme = explode(':', $theme, 2)[0];
+    $theme = array_reverse(explode('/', $theme))[0];
+
+    if (isset($theme) && strlen($theme) > 0) {
+        return !empty($path)
+            ? public_path("themes/{$theme}/{$path}")
+            : public_path("themes/{$theme}");
+    }
 }
