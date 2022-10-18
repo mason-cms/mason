95bb2fcebe5213eea404da132c0493f4e00096d6
Jonathan Roy <jroy66@gmail.com>
2022-10-17 22:46:29 -0400

    init site in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $site = site();
+        View::share('site', $site = site());
 
         Blade::directive('i', function ($expression) {
             return "<?= i($expression); ?>";
