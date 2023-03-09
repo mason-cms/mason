56e7bf693e402d4cd42005a7d126a13c11c96984
Jonathan Roy <jroy66@gmail.com>
2023-03-09 11:35:41 -0500

    dev media shortcodes

--- app/Providers/ParserServiceProvider.php
+++ app/Providers/ParserServiceProvider.php
@@ -27,16 +27,38 @@ class ParserServiceProvider extends ServiceProvider
     {
         \Parser::registerShortcode('media-url', function (array $parameters = []) {
             if (isset($parameters['id'])) {
-                if ($medium = site()->medium($parameters['id'])) {
-                    return $medium->url;
-                }
+                $medium = site()->medium($parameters['id']);
             } elseif (isset($parameters['title'])) {
-                if ($medium = site()->media()->byTitle($parameters['title'])->first()) {
-                    return $medium->url;
+                $medium = site()->media()->byTitle($parameters['title'])->first();
+            }
+
+            return isset($medium)
+                ? $medium->url
+                : "";
+        });
+
+        \Parser::registerShortcode('media', function (array $parameters = []) {
+            if (isset($parameters['id'])) {
+                $medium = site()->medium($parameters['id']);
+            } elseif (isset($parameters['title'])) {
+                $medium = site()->media()->byTitle($parameters['title'])->first();
+            }
+
+            if (isset($medium)) {
+                if ($medium->is_image) {
+                    $class = $parameters['class'] ?? '';
+                    $width = $parameters['width'] ?? $medium->image_width;
+                    $height = $parameters['height'] ?? $medium->image_height;
+                    $alt = $parameters['alt'] ?? '';
+                    $loading = $parameters['loading'] ?? 'lazy';
+
+                    return "<img class=\"{$class}\" src=\"{$medium->url}\" width=\"{$width}\" height=\"{$height}\" alt=\"{$alt}\" loading=\"{$loading}\" />";
+                } else {
+                    return "<!-- media type not supported -->";
                 }
             }
 
-            return '';
+            return "<!-- media not found -->";
         });
     }
 }
