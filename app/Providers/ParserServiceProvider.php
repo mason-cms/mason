537311b367468c0216890c8eaac2024a514c14b5
Jonathan Roy <jroy66@gmail.com>
2023-03-09 11:14:02 -0500

    add shortcode to retrieve media url by id or title

--- app/Providers/ParserServiceProvider.php
+++ app/Providers/ParserServiceProvider.php
@@ -25,6 +25,18 @@ class ParserServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Parser::registerShortcode('media-url', function (array $parameters = []) {
+            if (isset($parameters['id'])) {
+                if ($medium = site()->medium($parameters['id'])) {
+                    return $medium->url;
+                }
+            } elseif (isset($parameters['title'])) {
+                if ($medium = site()->media()->byTitle($parameters['title'])->first()) {
+                    return $medium->url;
+                }
+            }
+
+            return '';
+        });
     }
 }
