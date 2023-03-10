0856d12b31c8d64a64f3164ac0b9a9fcecfa5195
Jonathan Roy <jroy66@gmail.com>
2023-03-10 13:29:11 -0500

    add migration for thumbnail storage key

---
+++ database/migrations/2023_03_10_092257_add_thumbnail_storage_key_column_to_media_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->string('thumbnail_storage_key', 255)->nullable()->after('image_height');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->dropColumn('thumbnail_storage_key');
+        });
+    }
+};
