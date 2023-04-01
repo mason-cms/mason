a27278cb571286917212a706b0bab58a940c5ad0
Jonathan Roy <jroy66@gmail.com>
2023-04-01 11:29:08 -0400

    accept 'any' on the frontend

--- routes/web/frontend.php
+++ routes/web/frontend.php
@@ -15,10 +15,10 @@ $entryTypeNames = $entryTypes->pluck('name')->join('|');
 $taxonomyTypes = TaxonomyType::all();
 $taxonomyTypeNames = $taxonomyTypes->pluck('name')->join('|');
 
-Route::get('/', [FrontEndController::class, 'home'])
+Route::any('/', [FrontEndController::class, 'home'])
     ->name('home');
 
-Route::get('/{taxonomyType:name}/{taxonomy:name}/{entryType:name?}', [FrontEndController::class, 'taxonomy'])
+Route::any('/{taxonomyType:name}/{taxonomy:name}/{entryType:name?}', [FrontEndController::class, 'taxonomy'])
     ->where([
         'taxonomyType' => $taxonomyTypeNames,
         'taxonomy' => $alphanum,
@@ -26,13 +26,13 @@ Route::get('/{taxonomyType:name}/{taxonomy:name}/{entryType:name?}', [FrontEndCo
     ])
     ->name('taxonomy');
 
-Route::get('/{entryType:name}', [FrontEndController::class, 'entryType'])
+Route::any('/{entryType:name}', [FrontEndController::class, 'entryType'])
     ->where([
         'entryType' => $entryTypeNames,
     ])
     ->name('entryType');
 
-Route::get('/{entry:name}', [FrontEndController::class, 'entry'])
+Route::any('/{entry:name}', [FrontEndController::class, 'entry'])
     ->where([
         'entry' => $alphanum,
     ])
@@ -42,11 +42,11 @@ if (Schema::hasTable('locales')) {
     $locales = Locale::all();
     $localeNames = $locales->pluck('name')->join('|');
 
-    Route::get('/{locale:name}', [FrontEndController::class, 'home'])
+    Route::any('/{locale:name}', [FrontEndController::class, 'home'])
         ->where(['locale' => $localeNames])
         ->name('locale.home');
 
-    Route::get('/{locale:name}/{taxonomyType:name}/{taxonomy:name}/{entryType:name?}', [FrontEndController::class, 'taxonomy'])
+    Route::any('/{locale:name}/{taxonomyType:name}/{taxonomy:name}/{entryType:name?}', [FrontEndController::class, 'taxonomy'])
         ->where([
             'locale' => $localeNames,
             'taxonomy' => $alphanum,
@@ -55,14 +55,14 @@ if (Schema::hasTable('locales')) {
         ])
         ->name('locale.taxonomy');
 
-    Route::get('/{locale:name}/{entryType:name}', [FrontEndController::class, 'entryType'])
+    Route::any('/{locale:name}/{entryType:name}', [FrontEndController::class, 'entryType'])
         ->where([
             'locale' => $localeNames,
             'entryType' => $entryTypeNames,
         ])
         ->name('locale.entryType');
 
-    Route::get('/{locale:name}/{entry:name}', [FrontEndController::class, 'entry'])
+    Route::any('/{locale:name}/{entry:name}', [FrontEndController::class, 'entry'])
         ->where([
             'locale' => $localeNames,
             'entry' => $alphanum,
