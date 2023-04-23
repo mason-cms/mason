3a858bed523590987e19a1827b40b12f5097b980
Jonathan Roy <jroy66@gmail.com>
2023-04-23 12:31:34 -0400

    redirect users to /workshop after login instead of /backend

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/backend';
+    public const HOME = '/workshop';
 
     /**
      * The controller namespace for the application.
