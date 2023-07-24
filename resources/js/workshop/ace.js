b8f7ed0937cceeb86e2a30b37538b230be691043
Jonathan Roy <jroy66@gmail.com>
2023-07-24 13:12:53 -0400

    put base64 content/html in data- to prevent flashing it on screen

--- resources/js/workshop/ace.js
+++ resources/js/workshop/ace.js
@@ -6,8 +6,8 @@
 $(document).ready(function () {
     $('.ace-editor').each(function () {
         let $this = $(this).hide(),
-            base64Html = $this.html(),
-            html = base64Decode(base64Html),
+            base64 = $this.data('base64'),
+            html = base64Decode(base64),
             editorMode = $this.data('editor-mode'),
             editorMaxLines = $this.data('editor-max-lines') || $this.attr('rows') || 30,
             input = $this.data('input'),
@@ -35,12 +35,12 @@ $(document).ready(function () {
         editor.setValue(html, -1);
 
         if ($input && $input.length > 0) {
-            $input.val(base64Html);
+            $input.val(base64);
 
             editor.session.on('change', function() {
                 html = editor.session.getValue();
-                base64Html = base64Encode(html);
-                $input.val(base64Html);
+                base64 = base64Encode(html);
+                $input.val(base64);
             });
         }
     });
